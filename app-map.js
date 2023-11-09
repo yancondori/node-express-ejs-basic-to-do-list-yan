70303025701d0bbe6201c4a751dab371919e9923
fjbanezares <fjbanezares@gmail.com>
2023-11-09 15:37:48 +0100

    prepare to deploy

--- app-map.js
+++ app-map.js
@@ -121,5 +121,5 @@ app.get("/about", function (req, res) {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
-  console.log("Server started on port 3000");
+  console.log("Server started on port ${PORT}");
 });
