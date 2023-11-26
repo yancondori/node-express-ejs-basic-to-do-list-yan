59d48ee298cbaaa5dd2d83feed08f69bcd5c41c1
yancondori <yan.condori@gmail.com>
2023-11-26 20:53:20 +0100

    cambios para tarea

--- app-map.js
+++ app-map.js
@@ -40,9 +40,9 @@ const workItems = new Map();
 
 // Add some initial items to the Map
 // Seeding the map with initial values to simulate a pre-existing list.
-items.set('1', 'Buy Food');
-items.set('2', 'Cook Food');
-items.set('3', 'Eat Food');
+items.set('1', 'Study Data Architucture');
+items.set('2', 'Study Node Run commands');
+items.set('3', 'Go to the gym');
 
 // Define a route handler for the root route (homepage).
 // This home route handler displays the current to -do list items by date.
